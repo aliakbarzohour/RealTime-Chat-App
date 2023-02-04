662e6281d40fc37d55945e70334f6c3129e8a0c4
AliAkbar Zohour <71524940+aliakbarzohour@users.noreply.github.com>
2023-02-05 01:04:16 +0330

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -1,65 +1,61 @@
 <?php
-session_start();
-
-include_once "config.php";
-$fname = mysqli_real_escape_string($conn, $_POST['fname']);
-$lname = mysqli_real_escape_string($conn, $_POST['lname']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
-
-if (!empty($fname) && !empty($lname) && !empty($email) && !empty($password)) {
-    // check user email is valid or not
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // check that email already exist in the database or not
-        $sql = mysqli_query($conn, "SELECT email FROM users WHERE email = '{$email}'");
-        if (mysqli_num_rows($sql) > 0) {
-            // if email already exist
-            echo "$email - this email already exist";
-        } else {
-            // check user upload file or not
-            if (isset($_FILES['image'])) {
-                $img_name = $_FILES['image']['name'];
-                $tmp_name = $_FILES['image']['tmp_name'];
-
-                $img_explode = explode('.', $img_name);
-                $img_ext = end($img_explode);
-
-                $extensions = ['png', 'jpeg', 'jpg'];
-
-                if (in_array($img_ext, $extensions) === true) {
-                    $time = time();
-
-                    $new_img_name = $time . $img_name;
-                    if (move_uploaded_file($tmp_name, "images/" . $new_img_name)) {
-                        // check user is online or not
-                        $status = "Active now";
-                        // create random Id for user
-                        $random_id = rand(time(), 100000000);
-
-                        // insert all user data inside table
-                        $sql2 = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status) VALUES ({$random_id}, '{$fname}', '{$lname}', '{$email}', '{$password}', '{$new_img_name}', '{$status}')");
-
-                        if ($sql2) {
-                            $sql3 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
-                            if (mysqli_num_rows($sql3) > 0) {
-                                $row = mysqli_fetch_assoc($sql3);
-                                $_SESSION['unique_id'] = $row['unique_id'];
-                                echo "success";
-                            } else {
-                                echo "Something went wrong !";
+    session_start();
+    include_once "config.php";
+    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+    $lname = mysqli_real_escape_string($conn, $_POST['lname']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+            if(mysqli_num_rows($sql) > 0){
+                echo "$email - This email already exist!";
+            }else{
+                if(isset($_FILES['image'])){
+                    $img_name = $_FILES['image']['name'];
+                    $img_type = $_FILES['image']['type'];
+                    $tmp_name = $_FILES['image']['tmp_name'];
+                    
+                    $img_explode = explode('.',$img_name);
+                    $img_ext = end($img_explode);
+    
+                    $extensions = ["jpeg", "png", "jpg"];
+                    if(in_array($img_ext, $extensions) === true){
+                        $types = ["image/jpeg", "image/jpg", "image/png"];
+                        if(in_array($img_type, $types) === true){
+                            $time = time();
+                            $new_img_name = $time.$img_name;
+                            if(move_uploaded_file($tmp_name,"images/".$new_img_name)){
+                                $ran_id = rand(time(), 100000000);
+                                $status = "Active now";
+                                $encrypt_pass = md5($password);
+                                $insert_query = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
+                                VALUES ({$ran_id}, '{$fname}','{$lname}', '{$email}', '{$encrypt_pass}', '{$new_img_name}', '{$status}')");
+                                if($insert_query){
+                                    $select_sql2 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
+                                    if(mysqli_num_rows($select_sql2) > 0){
+                                        $result = mysqli_fetch_assoc($select_sql2);
+                                        $_SESSION['unique_id'] = $result['unique_id'];
+                                        echo "success";
+                                    }else{
+                                        echo "This email address not Exist!";
+                                    }
+                                }else{
+                                    echo "Something went wrong. Please try again!";
+                                }
                             }
+                        }else{
+                            echo "Please upload an image file - jpeg, png, jpg";
                         }
+                    }else{
+                        echo "Please upload an image file - jpeg, png, jpg";
                     }
-                } else {
-                    echo "Please select an image file - jpeg, jpg, png !";
                 }
-            } else {
-                echo "Please select an image file !";
             }
+        }else{
+            echo "$email is not a valid email!";
         }
     }else{
-        echo "$email - This is Not a Valid Email !";
+        echo "All input fields are required!";
     }
-} else {
-    echo "All Input Field Are Required !";
-}
+?>
