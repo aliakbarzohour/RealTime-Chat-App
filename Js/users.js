cf7f24e6c168a5d340484bdfa273b0f279acf591
aliakbarzohour <aliakbar.zohour@gmail.com>
2023-02-04 12:43:45 +0330

    search bar Js Code

---
+++ Js/users.js
@@ -0,0 +1,9 @@
+// Search
+const searchBar = document.querySelector(".users .search input"),
+        searchBtn = document.querySelector(".users .search button");
+    
+searchBtn.onclick =()=>{
+    searchBar.classList.toggle("active");
+    searchBar.focus();
+    searchBtn.classList.toggle("active");
+}
