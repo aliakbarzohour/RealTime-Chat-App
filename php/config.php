c939d42b79e7846a11ed5449cd0f4a7700855df0
aliakbarzohour <aliakbarzohour@gmail.com>
2023-02-06 00:42:18 +0330

    new config

--- php/config.php
+++ php/config.php
@@ -1,2 +1,11 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "chat");
+  $hostname = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "chatapp";
+
+  $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  if(!$conn){
+    echo "Database connection error".mysqli_connect_error();
+  }
+?>
